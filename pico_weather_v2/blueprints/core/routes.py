81b5a2ffef18216a37917298e92c1a4b28ddff92
zNitche <53448802+zNitche@users.noreply.github.com>
2023-06-07 17:21:19 +0200

    checking if logs are being saved

--- pico_weather_v2/blueprints/core/routes.py
+++ pico_weather_v2/blueprints/core/routes.py
@@ -12,14 +12,23 @@ def home(request):
 
 @core.route("/weather_logs", methods=["GET"])
 def logged_weather_days(request):
-    return routes_utils.render_template(core.get_template_path("weather_days.html"), {})
+    performing_log = core.current_app.weather_logger.performing_log
+
+    if performing_log:
+        return routes_utils.render_template(core.get_template_path("logging_in_progress.html"), {})
+    else:
+        return routes_utils.render_template(core.get_template_path("weather_days.html"), {})
 
 
 @core.route("/weather_logs/<day>", methods=["GET"])
 def weather_logs(request):
     day = request.path_parameters.get("day")
+    performing_log = core.current_app.weather_logger.performing_log
 
     if day is None:
         return core.current_app.raise_error(404)
 
-    return routes_utils.render_template(core.get_template_path("weather_day.html"), {"day": day})
+    if performing_log:
+        return routes_utils.render_template(core.get_template_path("logging_in_progress.html"), {})
+    else:
+        return routes_utils.render_template(core.get_template_path("weather_day.html"), {"day": day})
